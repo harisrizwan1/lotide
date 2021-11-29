22e553804f621b91b834990984b02df5ef1d95a6
harisrizwan1 <harisrizwan985@gmail.com>
2021-11-29 12:49:59 -0600

    added tests for assertequal and head

--- assertEqual.js
+++ assertEqual.js
@@ -7,6 +7,3 @@ const assertEqual = function(actual, expected) {
 };
 
 module.exports = assertEqual;
-
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
--- head.js
+++ head.js
@@ -4,6 +4,4 @@ const head = function(input) {
   return input[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
+module.exports = head;
